b1885950c69bb3293dad9ff478667f09501b3154
Brian Wolf <bw@activustech.com>
2016-07-11 16:41:01 -0400

    Moved to src directory

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import os
+from distutils.core import setup
+
+
+classifiers = """\
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Programming Language :: Python
+Topic :: Internet :: File Transfer Protocol (FTP)
+Operating System :: Unix, Linux, Windows, Mac
+"""
+
+def read(*rel_names):
+    return open(os.path.join(os.path.dirname(__file__), *rel_names)).read()
+
+
+setup(
+    name='sftpserver',
+    version='0.3',
+    url='http://github.com/global-knowledge/sftp-server',
+    license='MIT',
+    install_requires=['distribute', 'paramiko'],
+    packages=['distutils', 'distutils.command'],
+    py_modules = ['stuf_sftp'],
+    zip_safe=False,
+    entry_points="""\
+    [console_scripts]
+    sftpserver = sftpserver:main
+    """,
+    classifiers=filter(None, classifiers.split('\n')),
+    description='sftp-server - a simple single-threaded sftp server',
+    long_description=read('README'),
+    )
